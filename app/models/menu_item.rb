6fa497df5a104abbe5b8577b018fd35999a04f4a
swade781977 <swade781977@yahoo.com>
2021-01-10 03:04:20 -0500

    It;s done

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,6 @@
 class MenuItem < ApplicationRecord
-    belongs_to :line_item
-    has_many :orders, through: :line_items 
+    
+    
 
     CATEGORY = ['Pizza', 'Side', 'Drink', 'Dessert', 'Wings']
 end
